44646f71c2fcdffb8fea77ceb2e13a92f301e6a5
Sage Raflik <neonfuz@gmail.com>
2016-01-14 01:55:47 -0600

    Fixed correctness of return value

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
 {
     if(argc < 2) {
         fputs("No file specified\n", stderr);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     /* Open the file */
