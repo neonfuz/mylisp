1e65c05bdc19645fb1866eb7b309818e186587b1
Sage Raflik <neonfuz@gmail.com>
2016-01-14 02:20:00 -0600

    Added non working parsing

--- main.c
+++ main.c
@@ -6,6 +6,51 @@
 #include <sys/mman.h>
 #include <assert.h>
 
+typedef struct {
+    int start, end, depth;
+} Entry;
+
+void print_entry(Entry e)
+{
+    printf("%d, %d, %d\n", e.start, e.end, e.depth);
+}
+
+/* Gets length in entries of program 'program' of size 'proglen' bytes */
+size_t get_len(char *program, size_t proglen)
+{
+    size_t i, len = 0;
+    for(i=0; i<proglen; ++i) {
+        if(program[i] == '(')
+            ++len;
+    }
+    return len;
+}
+
+/*
+ * Parses program 'program' of size 'proglen' bytes, into 'entries', of
+ * length 'len' entries. Is unsafe on misformatted programs.
+ */
+int parse(char *program, size_t proglen, Entry *entries, size_t len)
+{
+    int depth = 0;
+    size_t entry_i = 0;
+
+    size_t i;
+    for(i=0; i<proglen; ++i) {
+        if(program[i] == '(') {
+            ++depth;
+            entries[entry_i].start = i;
+            entries[entry_i].depth = depth;
+            ++entry_i;
+        }
+        if(program[i] == ')')
+            entries[entry_i-depth].end = i;
+            --depth;
+    }
+
+    return entry_i;
+}
+
 int main(int argc, char *argv[])
 {
     if(argc < 2) {
@@ -26,10 +71,16 @@ int main(int argc, char *argv[])
     char *program = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
     assert(program != MAP_FAILED);
 
-    /* Print the program */
-    long i;
-    for(i=0; i<sb.st_size; ++i)
-        putchar(program[i]);
+    /* Parse file */
+    size_t len = get_len(program, sb.st_size);
+    Entry *entries = malloc(sizeof(Entry) * len);
+    parse(program, sb.st_size, entries, len);
+
+    /* Print file */
+    size_t i;
+    for(i=0; i<len; ++i) {
+        print_entry(entries[i]);
+    }
 
     /* Clean up */
     munmap(program, sb.st_size);
