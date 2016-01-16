a5a640ac4f4e9becaf3a4ec861ec30ec21968a5e
Sage Raflik <neonfuz@gmail.com>
2016-01-15 20:42:45 -0600

    Added more printing functionality to main.c

--- main.c
+++ main.c
@@ -17,6 +17,78 @@ void print_token(Token e, char *program)
     putchar('\n');
 }
 
+#define TABWIDTH 4
+
+void print_node(Node n, int depth)
+{
+    static const char *type_names[] = {
+        [NT_link] =   "link",
+        [NT_number] = "number",
+        [NT_string] = "string",
+        [NT_symbol] = "symbol",
+        [NT_bool] =   "bool",
+    };
+
+    char tabstr[depth*TABWIDTH + 1];
+    int i;
+    for(i=0; i<depth*TABWIDTH; ++i)
+        tabstr[i] = ' ';
+    tabstr[i] = '\0';
+
+    printf("%sType: %s\n", tabstr, type_names[n.type]);
+
+    switch(n.type) {
+    case NT_link:
+        printf("%s  Node: %p\n", tabstr, n.lin.node);
+        printf("%s  Next: %p\n", tabstr, n.lin.next);
+        LinkNode *next;
+        for ( next=&n.lin; next != NULL; next=next->next ) {
+            print_node( *next->node, depth+1 );
+        }
+        break;
+    case NT_symbol:
+        printf("%s  Name: %s\n", tabstr, n.sym.name);
+        break;
+    case NT_string:
+        printf("%s  Value: \"%s\"\n", tabstr, n.str.value);
+        break;
+    case NT_number:
+        printf("%s  Value: [%d]\n", tabstr, n.num.value);
+        break;
+    case NT_bool:
+        printf("%s  Value: '%c'\n", tabstr, n.bol.value?'1':'0');
+        break;
+    }
+}
+
+void print_node_to_sexpr(Node n)
+{
+    switch(n.type) {
+    case NT_link:
+        putchar('(');
+        LinkNode *next;
+        for ( next=&n.lin; next != NULL; next=next->next ) {
+            print_node_to_sexpr( *next->node );
+            if(next->next != NULL)
+                putchar(' ');
+        }
+        putchar(')');
+        break;
+    case NT_symbol:
+        fputs(n.sym.name, stdout);
+        break;
+    case NT_string:
+        fputs(n.str.value, stdout);
+        break;
+    case NT_number:
+        printf("%d", n.num.value);
+        break;
+    case NT_bool:
+        putchar(n.bol.value?'1':'0');
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if(argc < 2) {
@@ -26,18 +98,40 @@ int main(int argc, char *argv[])
 
     MappedFile mf = map_file_rd(argv[1]);
 
+    /* Original program */
+    puts("Original program:");
+    size_t i;
+    for(i=0; i<mf.sb.st_size; ++i)
+        putchar(mf.file[i]);
+    puts("");
+
     /* Tokenize file */
     size_t len = count_tokens(mf.file, mf.sb.st_size);
     Token *tokens = malloc(sizeof(Token) * len);
     tokenize(mf.file, tokens);
 
     /* Print tokens */
+    puts("Tokenized program:");
     printf("start end chldrn\n");
-    size_t i;
     for(i=0; i<len; ++i) {
         print_token(tokens[i], mf.file);
     }
+    puts("");
 
+    /* Parse tokens */
+    Node n = parse_sexpr(tokens, mf.file);
+
+    /* Print nodes */
+    puts("Parsed tokens:");
+    print_node(n, 0);
+    puts("");
+
+    /* Parsed to sexpression */
+    puts("Parsed then re-printed as sexpr:");
+    print_node_to_sexpr(n);
+    puts("");
+
+    puts("");
     unmap_file(mf);
     return 0;
 }
