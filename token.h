49704d80eaddd8a2e17d3a4cd72cfa1f2ccbbc9e
Sage Raflik <neonfuz@gmail.com>
2016-01-15 18:04:48 -0600

    Fixed count_tokens name in token.h

--- token.h
+++ token.h
@@ -5,7 +5,7 @@ typedef struct {
     int start, end, children;
 } Token;
 
-size_t count(char *program, size_t proglen);
+size_t count_tokens(char *program, size_t proglen);
 size_t tokenize_rec(char *program, Token *tokens,
                     size_t prog_i, size_t *ent_i);
 void tokenize(char *program, Token *tokens);
